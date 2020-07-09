d783ad9fec307a74ac858d81f6491b9824f69789
Isaac Mthimunye <imthimun@c4r6s5.wethinkcode.co.za>
2020-07-09 11:35:23 +0200

    done with numbers

---
+++ ex02/ft_print_numbers.c
@@ -0,0 +1,18 @@
+
+ void ft_putchar( char c);
+
+void ft_print_numbers(void)
+{
+	int current_number ='0';
+
+	for( int i=0 ; i < 10 ; i++ )
+	{
+			ft_putchar(current_number);
+
+			current_number++;
+	}
+}
+
+
+
+
